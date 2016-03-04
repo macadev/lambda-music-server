d3a48f6b910fda2cc9221d07e0d8c6d5fba11fbd
macadev <danielmacario5@gmail.com>
2016-03-04 14:47:38 -0500

    add leading underscore to global variables

--- lambdamusic.py
+++ lambdamusic.py
@@ -13,10 +13,10 @@ application = Flask(__name__)
 application.secret_key = 'some secret key'
 application.config['SESSION_TYPE'] = 'filesystem'
 
-process_pool = None
+_process_pool = None
 manager = Manager()
-shared_queue = manager.list()
-playing = manager.Value('i', 0)
+_shared_queue = manager.list()
+_playing = manager.Value('i', 0)
 
 @application.route("/")
 def hello():
@@ -24,7 +24,7 @@ def hello():
 
 @application.route("/submit", methods=['POST'])
 def submit_song():
-    global playing
+    global _playing
     print("Processing incoming song request")
     song_url = request.form['song_url']
     print(song_url)
@@ -40,27 +40,26 @@ def submit_song():
     else:
         return "Invalid URL. Only Youtube and Soundcloud links allowed!"
 
-    if playing.value is 0:
-        print('playing is 0!!!')
-        playing.value = 1
+    if _playing.value is 0:
+        _playing.value = 1
         # How do I apply a callback without passing retval down the chain?
-        process_pool.apply_async(add_song_to_queue, [song_url, name, source_type], callback=run_through_queue)
+        _process_pool.apply_async(add_song_to_queue, [song_url, name, source_type], callback=run_through_queue)
     else:
-        process_pool.apply_async(add_song_to_queue, [song_url, name, source_type])
+        _process_pool.apply_async(add_song_to_queue, [song_url, name, source_type])
 
-    print(len(shared_queue))
+    print(len(_shared_queue))
     flash('Song added successfully!')
     return render_template('index.html')
 
 @application.route("/playlist", methods=['GET', 'POST'])
 def get_playlist():
-    global playing
+    global _playing
     playlist = []
     i = 0
-    queue_size = len(shared_queue)
+    queue_size = len(_shared_queue)
     while i < queue_size:
-        print(shared_queue[i])
-        playlist.append(shared_queue[i])
+        print(_shared_queue[i])
+        playlist.append(_shared_queue[i])
         i = i + 1
     return json.dumps(playlist)
 
@@ -78,28 +77,28 @@ def add_song_to_queue(song_url, submitter_name, source_type):
         song_info = Downloader.download_youtube_song(song_url, submitter_name)
     else:
         song_info = Downloader.download_soundcloud_song(song_url, submitter_name)
-    shared_queue.append(song_info)
-    print(len(shared_queue))
+    _shared_queue.append(song_info)
+    print(len(_shared_queue))
 
 def run_through_queue(_):
     try:
         while True:
-            song = shared_queue[0]
+            song = _shared_queue[0]
             print('Playing song!', song.get('filename'))
             subprocess.call(['mpg123', song.get('filename')])
             # delete the song from queue
             print("Deleting song!")
             os.remove(song.get('filename'))
-            del shared_queue[0]
+            del _shared_queue[0]
     except:
         print("Unexpected error:", sys.exc_info())
-        playing.value = 0
+        _playing.value = 0
         pass
 
 if __name__ == "__main__":
-    process_pool = Pool()
+    _process_pool = Pool()
     try:
         application.run(host='0.0.0.0', debug=True)
     except KeyboardInterrupt:
-        process_pool.close()
-        process_pool.join()
+        _process_pool.close()
+        _process_pool.join()
